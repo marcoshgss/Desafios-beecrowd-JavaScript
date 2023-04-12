05706b7d1429eede68f1ef662ec7196ec069cffd
Marcos Henrique <sena_marcos.fp@hotmail.com>
2023-04-12 06:10:45 -0300

    comite e update README-PROBLEMA 9

--- Problema 9/1009.js
+++ Problema 9/1009.js
@@ -1,23 +1,9 @@
-// Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas 
-// efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre 
-// suas vendas efetuadas, informar o total a receber no final do mês, com duas casas decimais.
-
 // nomeVendedor
 // salarioFixo
 // totalVendas
 
 // total
 // var A = parseFloat(lines.shift());
-/* function problem (values) {
-    const [nomeVendedor, salarioFixo, totalVendas] = values
-     
-
-    const total = Number(salarioFixo) + (Number(totalVendas) * 0.15)
-    console.log(`TOTAL = R$ ${total.toFixed(2)}`)
-    return values
-}
-console.log(problem['JOÃO', 500.00, 1230.30])
-*/
 
 // Código 1, enviei esse código para o Beecrowd.
 /*
@@ -29,6 +15,47 @@ const totall = (salario) + (totalVendass) * 0.15
 console.log(`TOTAL = R$ ${totall.toFixed(2)}`)
 */
 
+
+// Outro código
+const nome1 = 'JOÃO'
+const salario1 = 500.00
+const totalVendass1 = 1230.30
+
+const totall1 = (salario1) + ((totalVendass1) * 0.15)
+console.log(`TOTAL = R$ ${totall1.toFixed(2)}`)
+
+// Outro código
+function salarioBonus(nomeV, salarioo, total_vendas) {
+    const totalTudo = Number(salarioo) + (Number(total_vendas) * 0.15)
+    return (`TOTAL = R$ ${totalTudo.toFixed(2)}`)
+}
+
+console.log(salarioBonus('PEDRO', 700.00, 0.00))
+
+// Outro código
+function problem(values) {
+    const [nomeVendedor, salarioFixo, totalVendas] = values
+
+    const total = Number(salarioFixo) + (Number(totalVendas) * 0.15)
+    return (`TOTAL = R$ ${total.toFixed(2)}`)
+}
+
+values = ['MANGOJATA', 1700.00, 1230.50]
+console.log(problem(values))
+
+/* function problem (values) {
+    const [nomeVendedor, salarioFixo, totalVendas] = values
+     
+
+    const total = Number(salarioFixo) + (Number(totalVendas) * 0.15)
+    console.log(`TOTAL = R$ ${total.toFixed(2)}`)
+    return values
+}
+console.log(problem['JOÃO', 500.00, 1230.30])
+*/
+
+
+
 /*
 // Outro código
 function problem (values) {
@@ -39,10 +66,3 @@ function problem (values) {
     console.log(`TOTAL = R$ ${total.toFixed(2)}`)
 }
 */
-
-const nome1 = 'JOÃO'
-const salario1 = 500.00
-const totalVendass1 = 1230.30
-
-const totall1 = (salario1) + ((totalVendass1) * 0.15)
-console.log(`TOTAL = R$ ${totall1.toFixed(2)}`)
